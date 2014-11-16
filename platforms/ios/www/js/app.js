b0797f445cf4d7d623296efdf50f26f56fc225b0
jameswang14 <james.jwang14@gmail.com>
2014-11-15 22:38:56 -0500

    fixed appjs

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -1,72 +1,22 @@
 (function(){
-  'use strict';
-  var module = angular.module('app', ['onsen']);
-
-  module.controller('AppController', function($scope, $data) {
-    $scope.doSomething = function() {
-      setTimeout(function() {
-        alert('tappaed');
-      }, 100);
-    };
-  });
-
-  module.controller('GetMatchController', function($scope, $http) {
-      $scope.data = {};
-
-      $scope.data.click= function (item, event) {
+  'use strict'
+  angular.module('app', ['onsen']).controller(
+    'GetMatchController', ['$scope', '$http', function($scope, $http) {
 
     $http.get(
-      url: "https://www.kimonolabs.com/api/4ag7ique?apikey=BsZyX8bXMCMKLt0LIyhOVkKv91tYUfLQ"
-    ).success(function(data)){
-      $scope.myData = data;
+      "https://www.kimonolabs.com/api/4ag7ique?apikey=BsZyX8bXMCMKLt0LIyhOVkKv91tYUfLQ").
+    success(function(data){
+      $scope.results = data;
     }).error(function(){
-
+      $scope.results = "fail";
     });
 
-    });
 
-  module.controller('DetailController', function($scope, $data) {
-    $scope.item = $data.selectedItem;
-  });
-
-  module.controller('MasterController', function($scope, $data) {
-    $scope.items = $data.items;  
-    
-    $scope.showDetail = function(index) {
-      var selectedItem = $data.items[index];
-      $data.selectedItem = selectedItem;
-      $scope.ons.navigator.pushPage('detail.html', {title : selectedItem.title});
-    };
-  });
+    }
+    ]);
 
 
 
-      var data = {};
       
-      data.items = [
-          { 
-              title: 'Item 1 Title',
-              label: '4h',
-              desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-          },
-          { 
-              title: 'Another Item Title',
-              label: '6h',
-              desc: 'Ut enim ad minim veniam.'
-          },
-          { 
-              title: 'Yet Another Item Title',
-              label: '1day ago',
-              desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-          },
-          { 
-              title: 'Yet Another Item Title',
-              label: '1day ago',
-              desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-          }
-      ]; 
-      
-      return data;
-  });
-})();
+}());
 
--- www/js/app1.js
+++ www/js/app1.js
@@ -1,31 +1,19 @@
 (function(){
   'use strict'
-  var teams = [];
-  var module = angular.module('app', ['onsen']);
-  module.controller(
+  angular.module('app', ['onsen']).controller(
     'GetMatchController', ['$scope', '$http', function($scope, $http) {
-
-    $http.get(
+    /**$http.get(
       "https://www.kimonolabs.com/api/4ag7ique?apikey=BsZyX8bXMCMKLt0LIyhOVkKv91tYUfLQ").
     success(function(data){
-      $scope.results = "ljj";
+      $scope.results = data.results.collection1[0].Team2.text;
     }).error(function(){
       $scope.results = "fail";
-    });
+    });*/
 
 
     }
     ]);
-    module.controller(
-    'AddTeamController', ['#scope', '$data', function($scope, $data)
-    {
 
-
-    }
-
-
-
-    ]);
 
 
       
