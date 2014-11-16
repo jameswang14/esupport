b9132131f8ed7cca8e17596801991e09227c9945
Rossem <rrsbrg@gmail.com>
2014-11-16 02:46:41 -0500

    dongerino iconerino

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -1,12 +1,14 @@
 (function(){
   'use strict'
-  angular.module('app', ['onsen']).controller(
+  var teams = [];
+  var module = angular.module('app', ['onsen']);
+  module.controller(
     'GetMatchController', ['$scope', '$http', function($scope, $http) {
 
     $http.get(
       "https://www.kimonolabs.com/api/4ag7ique?apikey=BsZyX8bXMCMKLt0LIyhOVkKv91tYUfLQ").
     success(function(data){
-      $scope.results = data;
+      $scope.results = "ljj";
     }).error(function(){
       $scope.results = "fail";
     });
@@ -14,7 +16,16 @@
 
     }
     ]);
+    module.controller(
+    'AddTeamController', ['#scope', '$data', function($scope, $data)
+    {
 
+
+    }
+
+
+
+    ]);
 
 
       
